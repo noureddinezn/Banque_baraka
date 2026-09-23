b121485df94c04e735509fd760d8aad9fed273a0
noureddinezn <noureddinezouana@gmail.com>
2026-09-23 09:20:32 +0000

    la creation des la class Compte est un class abstract

--- src/entity/Compte.java
+++ src/entity/Compte.java
@@ -1,5 +1,27 @@
-package src.entity;
+package entity;
 
-public class Compte {
-    
+public abstract class Compte {
+    private int id;
+    private String numero;
+    private double solde;
+    private int idClient;
+
+    public Compte(int id, String numero, double solde, int idClient) {
+        this.id = id;
+        this.numero = numero;
+        this.solde = solde;
+        this.idClient = idClient;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNumero() { return numero; }
+    public void setNumero(String numero) { this.numero = numero; }
+
+    public double getSolde() { return solde; }
+    public void setSolde(double solde) { this.solde = solde; }
+
+    public int getIdClient() { return idClient; }
+    public void setIdClient(int idClient) { this.idClient = idClient; }
 }
